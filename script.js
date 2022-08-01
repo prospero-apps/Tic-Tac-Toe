018deae295a28f38dc352403847e9e746a7cb942
Kamil <prospero.apps@gmail.com>
2022-08-01 14:10:56 +0200

    Create object outlines in js

---
+++ script.js
@@ -0,0 +1,46 @@
+/* GAME*/
+// mode (human vs human or human vs computer)
+// select game mode
+// create players
+// player1
+// player2
+// player in control
+// gameboard
+// display controller
+// round number
+// check game over (3 in a row/column/diagonal and tie)
+// start game
+// start new round
+// play again
+// set winner
+// switch control to the other player
+// AI - let computer make first random move
+// AI - implement minimax for computer
+
+/* GAMEBOARD*/
+// create array of squares
+// clear (remove all markers and enable all squares)
+
+/* DISPLAY CONTROLLER*/
+// collect DOM elements
+// render (after each marker is added)
+// add marker to square
+// display winner
+// hide/unhide select players panel
+// hide/unhide player settings panel
+// hide/unhide winner panel
+
+/* PLAYER*/
+// name
+// score
+// marker (X or O)
+// markers (array of all markers)
+// isWinner
+// put marker (only if square is not taken)
+
+
+/* SQUARE*/
+// content (X or O)
+// taken (if there's already an X or O)
+// position (index in the array of squares)
+// check if taken
