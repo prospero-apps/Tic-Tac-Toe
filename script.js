ad3a64370416af3d40dd8b1bd99c33c63fce9a5b
Kamil <prospero.apps@gmail.com>
2022-08-01 17:17:37 +0200

    Create the displayController module

--- script.js
+++ script.js
@@ -37,13 +37,100 @@ const gameboard = (() => {
 })();
 
 /* DISPLAY CONTROLLER*/
-// collect DOM elements
-// render (after each marker is added)
-// add marker to square
-// display winner
-// hide/unhide select players panel
-// hide/unhide player settings panel
-// hide/unhide winner panel
+const displayController = (() => {
+    // DOM elements
+    // panels
+    const selectPanel = document.getElementById('select');
+    const settingsPanel = document.getElementById('player-settings');
+    const player1Panel = document.getElementById('player1area');
+    const player2Panel = document.getElementById('player2area'); 
+    const gameboardPanel = document.getElementById('main');
+    const winnerPanel = document.getElementById('winner');
+
+    // Select Panel
+    const hvhButton = document.getElementById('hvh');
+    const hvcButton = document.getElementById('hvc');
+
+    // Player Settings Panel
+    const player1Input = document.getElementById('p1-name');
+    const player2Input = document.getElementById('p2-name');
+
+    // Player Areas
+    const player1Label = document.getElementById('p1-label');
+    const player1Score = document.getElementById('p1-score');
+    const player2Label = document.getElementById('p2-label');
+    const player2Score = document.getElementById('p2-score');
+
+    // Gameboard
+    const gb = document.getElementById('main');
+    const squareFields = document.querySelectorAll('.square');
+
+    // Winner Panel
+    const winnerLabel = document.getElementById('winner-text');
+    const newRoundButton = document.getElementById('new-round');
+    const newGameButton = document.getElementById('new-round');
+
+    // hiding and showing panels
+    const hidePanel = (panel) => {
+        panel.style.display = 'none';
+    }
+
+    const showPanel = (panel) => {
+        panel.style.display = 'block';
+    }
+
+    const addMarker = (square, marker) => {
+        square.textContent = marker;
+    }
+
+    const clearMarker = (square) => {
+        square.textContent = '';
+    }
+
+    const clearGb = () => {
+        for(const square of squareFields) {
+            clearMarker(square);
+        }
+    }
+
+    const updateScores = (player1, player2) => {
+        player1Score.textContent = player1.score;
+        player2Score.textContent = player2.score;
+    }
+
+    const updatePlayerNames = (player1, player2) => {
+        player1Label.textContent = player1.name;
+        player2Label.textContent = player2.name;
+    }
+
+    return {
+        selectPanel,
+        settingsPanel,
+        player1Panel,
+        player2Panel,
+        gameboardPanel,
+        winnerPanel,
+        hvhButton,
+        hvcButton,
+        player1Input, 
+        player2Input,
+        player1Label,
+        player1Score, 
+        player2Label, 
+        player2Score,
+        gb,
+        squareFields,
+        winnerLabel,
+        newRoundButton,
+        newGameButton,
+        hidePanel,
+        showPanel,
+        addMarker,
+        clearGb,
+        updateScores,
+        updatePlayerNames
+    };
+})();
 
 /* PLAYER*/
 const player = (name, marker) => {
